4aa84af112c16afe71068ba5e5f9a65920a8c21a
zf2169 <zf2169@columbia.edu>
2017-04-07 12:36:15 -0400

    lda try

--- lib/LDA.R
+++ lib/LDA.R
@@ -12,9 +12,52 @@ corpus<- function(llist){
   
   return(lapply(llist, document))
 }
-
 doc_corpus<- lapply(data_list, corpus)
 
-# arthor_name<- substring(names(lllist)[1], 1, nchar(names(lllist)[1])-2)
+allwords_fun<- function(llist) {
+  allwords_fun1<- function(lllist) {
+    return(c(lllist$name, lllist$words))
+  }
+  return(lapply(llist, allwords_fun1))
+}
+doc_allwords<- lapply(doc_corpus, allwords_fun)
+
+vocab_fun<- function(llist) {
+  vocab<- c()
+  n<- length(llist)
+  for(i in 1:n){
+    vocab<- c(vocab,llist[[i]])
+  }
+  vocab<- unique(vocab)
+  return(vocab)
+}
+vocab<- lapply(doc_allwords, vocab_fun)
+
+index<- function(x,a) {return(which(a==x))}
+doc_format<- vector("list", 14)
+for(i in 1:14) {
+  format_fun<- function(lllist) {
+    t<- table(lllist)
+    vec1<- sapply(names(t), index, a=vocab[[i]])-1
+    vec2<- as.numeric(t)
+    m<- matrix(as.integer(c(vec1, vec2)), ncol = 2)
+    return(t(m))
+  }
+  doc_format[[i]] <- lapply(doc_allwords[[i]], format_fun)
+}
+names(doc_format)<- query.list
+
+k<- 10
+try<- lda.collapsed.gibbs.sampler(doc_format$`A Gupta`,k, vocab$`A Gupta`,
+                            num.iterations = 500, alpha = k/50, eta = 0.01)
+
+
+
+
+
+
+
+
+
 
 
