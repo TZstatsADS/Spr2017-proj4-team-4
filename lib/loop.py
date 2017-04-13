2ea9f5e18fabb8a61cb85266aead65a9fbce3187
Zishuo (Jason)  Li <zl2528@columbia.edu>
2017-04-13 17:09:38 -0400

    Add files via upload

--- lib/loop.py
+++ lib/loop.py
@@ -4,14 +4,59 @@ Created on Thu Apr 13 11:56:24 2017
 
 @author: ZISHUO LI
 """
+#这是这俩分的情况下的ts
+def Ts(x,y):
+    ts_1_x=np.where(dtm_index_copy==group_unique[x])
+    ts_1_y=np.where(dtm_index_copy==group_unique[y])        
+    answer=cor(dtm.iloc[ts_1_x[0],:],dtm.iloc[ts_1_y[0],:])
+    group_unique_left=[m for m in group_unique if m!=group_unique[x] and m!=group_unique[y]]
+    for p in range(len(group_unique_left)):
+        for l in range(p+1,len(group_unique_left)):
+            ts_1_x=np.where(dtm_index_copy==group_unique[x])
+            ts_1_y=np.where(dtm_index_copy==group_unique[y])        
+            inter=cor(dtm.iloc[ts_1_x[0],:],dtm.iloc[ts_1_y[0],:])
+            answer+=inter
+    return (answer)
 
-        ts=np.repeat(0,dtm.shape[1])
 
-                ts=ts+var.values
+def group_precision(x,y):
+    g1=np.where(dtm_index_copy==group_unique[x])
+    g2=np.where(dtm_index_copy==group_unique[y])        
+    dtm_index_copy2=dtm_index_copy.copy()
+    answer=[]
+    for o in range(len(dtm_index_copy2)):
+        if dtm_index_copy2[o]!=group_unique[y] :
+            answer.append(dtm_index_copy2[o])
+        else:
+            answer.append(group_unique[x])
+    return (answer)
+    
+def precision(group):
+    A=0
+    B=0
+    C=0
+    D=0
+    for i in range(len(group)):
+        for j in range(len(group)):
+            if group[i]==group[j] and label[i]==label[j]:
+                A+=1
+            elif group[i]==group[j] and label[i]!=label[j]:
+                B+=1
+            elif group[i]!=group[j] and label[i]==label[j]:
+                C+=1
+            else:
+                D+=1
+    
+    Precision=A/(A+B)
+    Recall=A/(A+C)
+    F1=2*Precision*Recall/(Precision+Recall)
+    Accuracy=(A+D)/(A+B+C+D)
+    print(' '.join(['Accuracy:',str(Accuracy)]))
+    print(' '.join(['Precision:',str(Precision)])) 
+    return (Precision)  
+    
 
-       ts_=pd.inner(var_list,lamb)
-
-def mira(lamb,tao,ts,ts1):
+def mira(la,tao,ts,ts1,par=100):
     n_ft=len(lamb)
 
     accumulator = list()
@@ -21,46 +66,41 @@ def mira(lamb,tao,ts,ts1):
         return LA.norm([a-b for a,b in zip(x,lamb)])
 
     def constraint1(x):
-        return np.atleast_1d(np.inner(x,ts)-np.inner(x,ts1)-1)
+        return np.atleast_1d(-np.inner(x,ts)+np.inner(x,ts1)+par)
     def constraint2(x):
-        return np.atleast_1d(tao-np.inner(x,ts))
+        return np.atleast_1d(-tao+np.inner(x,ts))
 
     opt=optimize.fmin_slsqp(f, np.array([0]*n_ft),
                        ieqcons=[constraint1,constraint2, ])
     return(opt)
 
 
-def loop(dtm):
+
+def loop(dtm,tao=0.6  ):
+    global lamb
     group_unique=np.unique(dtm.index)
     cluster_number=group_unique.size
     cor_list=[]
     location_list=[]
     ''' loop first time  '''
     r=cluster_number
-    global var_list
     for i in range(r):
         for j in range(i+1,r):
             i_loc=np.where(dtm.index==group_unique[i])
             j_loc=np.where(dtm.index==group_unique[j])
-            var=cor(dtm.iloc[i_loc[0],:],dtm.iloc[j_loc[0],:])
-            
+            var=cor(dtm.iloc[i_loc[0],:],dtm.iloc[j_loc[0],:])         
             inter=score(var,lamb)
             cor_list.append(inter)
             location_list.append([i,j])
-    
- 
     
     cor_list=cor_list/np.mean(cor_list)
     location=np.array(np.where(cor_list==min(cor_list)))
     location_list=np.array(location_list)
     location_min=location_list[location]
     
-    ''' group part'''
-    
-    
-    
-    
+    ''' group part'''   
     group=[x for x in range(r)]
+    group_copy=group.copy()
     
     for i in range(location.size):
         num1=location_min[0][i][0]
@@ -80,40 +120,44 @@ def loop(dtm):
                 dtm_new.extend([x])
         ''' store dtm_new into dtm index'''
         dtm.index=dtm_new
+   
     '''accuration part'''
-    group=dtm.index.copy()
-    A=0
-    B=0
-    C=0
-    D=0
-    for i in range(len(group)):
-        for j in range(len(group)):
-            if group[i]==group[j] and label[i]==label[j]:
-                A+=1
-            elif group[i]==group[j] and label[i]!=label[j]:
-                B+=1
-            elif group[i]!=group[j] and label[i]==label[j]:
-                C+=1
-            else:
-                D+=1
-    #global Precision_previous    
-    Precision=A/(A+B)
-    #Precision_previous=Precision.copy()
-    Recall=A/(A+C)
-    F1=2*Precision*Recall/(Precision+Recall)
-    Accuracy=(A+D)/(A+B+C+D)    
-    print(' '.join(['Precision:',str(Precision)]))
-    print(' '.join(['Recall:',str(Recall)]))
-    print(' '.join(['F1:',str(F1)]))
-    print(' '.join(['Accuracy:',str(Accuracy)]))
     
-    #if Precision <=0.95:
+    group=dtm.index.copy() 
+    precision_st=precision(group)
+    
+    if precision_st <=0.95:
+        # get ts : total score without using paramter
+        ts_cluster=np.unique(dtm.index).size
+        group_unique_ts=np.unique(dtm.index)
+        ts=np.repeat(0,dtm.shape[1])
+        for m in range(ts_cluster):
+            for n in range(m+1,ts_cluster):
+                m_loc=np.where(dtm.index==group_unique_ts[m])
+                n_loc=np.where(dtm.index==group_unique_ts[n])
+                var=cor(dtm.iloc[m_loc[0],:],dtm.iloc[n_loc[0],:])
+                ts=ts+var.values
         
-        
+        for i in range(len(group_unique)):
+            for j in range(i+1,len(group_unique)):
+                    judge_precision=precision(group_precision(i,j))
+                    if judge_precision >precision_st:
+                        print([i,j])
+                        ts1=Ts(i,j)                 
+                        lamb=mira(la=lamb,tao=tao,ts=ts,ts1=ts1)  
+                        break
+            if judge_precision> precision_st:
+                break
+    print(np.unique(dtm.index).size)                
+    return (lamb) 
+
+
+
     
-    
-    
-    
-    return (np.unique(dtm.index).size) 
-        
-    
+dtm4=dtm.copy()
+np.unique(dtm2.index).size
+
+
+len(ts)
+kk=lamb.copy()
+np.equal(kk,lamb)
--- lib/prj4 2.py
+++ lib/prj4 2.py
@@ -4,23 +4,20 @@ Created on Mon Apr 10 20:40:39 2017
 
 @author: ZISHUO LI
 """
-# the best score:
-unique_cluster=np.unique([int(x) for x in content['author'].values])
-best_score_var=
-
-
 dtm=pd.read_csv('pca.csv')
-dtm.shape
+dtm=dtm.iloc[:,1:]
+
 
 
 dtm2=dtm.copy()
 dtm3=dtm.copy()
 
-dtm=dtm.iloc[:,1:]
+
 c=dtm.shape[1]
 r=dtm.shape[0]
 
 lamb=[1/c]*c
+
 label=[int(x) for x in content['author']]
 
 def cor(x,y):
@@ -29,11 +26,12 @@ def cor(x,y):
     return (correlation)    
 def score(var,lamb):
     return (sum(var*lamb))
-'''initial assignment'''
 
 
-for x in range(15):
-    loop()
+for x in range(5):
+    dtm2=loop(dtm2)
+
+dtm5=dtm2.copy()
 
 
 
